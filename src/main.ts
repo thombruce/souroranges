faec8b1f9a1c251ec0217060283ffc5851dd1f0b
Thom Bruce <thom@thombruce.com>
2023-04-15 02:05:23 +0100

    add Pinia store module

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import "./style.css"
 import App from './App.vue'
 import './samples/node-api'
 
+const pinia = createPinia()
+
 createApp(App)
+  .use(pinia)
   .mount('#app')
   .$nextTick(() => {
     postMessage({ payload: 'removeLoading' }, '*')
