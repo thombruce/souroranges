dc36e1b434ba9e5d46c5d5c76bcc8ed96e2d836a
Thom Bruce <thom@thombruce.com>
2023-04-16 23:28:12 +0100

    add fs-based persistence to Pinia store

--- src/store/entities.ts
+++ src/store/entities.ts
@@ -1,4 +1,7 @@
+import { ref } from "vue"
 import { defineStore } from "pinia"
+
+const loki = require("lokijs")
 
 interface Entity {
   item: string
@@ -6,19 +9,41 @@ interface Entity {
   completed: boolean
 }
 
-export const entities = defineStore("entityList", {
-  state: () => ({
-    entityList: [] as Entity[],
-    id: 0,
-  }),
-  actions: {
-    addEntity(item: string) {
-      this.entityList.push({ item, id: this.id++, completed: false })
-    },
-    deleteEntity(itemID: number) {
-      this.entityList = this.entityList.filter((object) => {
-        return object.id !== itemID
-      });
-    },
-  },
+export const entities = defineStore('entityList', () => {
+  // Setup
+  let db = new loki('marmalade.json', { autosave: true, autoload: true, autoloadCallback: initStore }), entitiesData: any
+
+  function initStore() {
+    entitiesData = db.getCollection('entities', { autoupdate: true })
+
+    if(!entitiesData){
+       entitiesData = db.addCollection('entities');
+    }
+
+    entityList.value.push(...entitiesData.data)
+  }
+
+  // State
+  const entityList = ref([] as Entity[])
+  const id = ref(0)
+
+  // Getters
+  // e.g. const doubleCount = computed(() => count.value * 2)
+
+  // Actions
+  function addEntity(item: string) {
+    let newEntity = { item, id: id.value++, completed: false }
+
+    entitiesData.insert(newEntity)
+    entityList.value.push(newEntity)
+  }
+
+  function deleteEntity(itemID: number) {
+    entitiesData.chain().find({ id: itemID }).remove()
+    entityList.value = entityList.value.filter((object) => {
+      return object.id !== itemID
+    })
+  }
+
+  return { entityList, id, addEntity, deleteEntity }
 })
