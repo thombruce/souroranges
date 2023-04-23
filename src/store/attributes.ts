e39409a0f48f06dee76075184404e6714b891246
Thom Bruce <thom@thombruce.com>
2023-04-23 21:05:00 +0100

    reference Loki collection as Pinia state in attributes

--- src/store/attributes.ts
+++ src/store/attributes.ts
@@ -1,64 +1,44 @@
 import { ref, computed } from "vue"
 import { defineStore } from "pinia"
 import { v4 as uuidv4 } from "uuid"
-import { unionBy as _unionBy } from 'lodash'
 
 import db from "../plugins/loki"
 
-interface Attribute {
-  id: string
-  entityID: string | string[]
-  attributeDefinitionID: string | string[]
-  value: string | number
-}
-
 export const useAttributesStore = defineStore('attributes', () => {
-  // Setup
-  let attributesData: any
-
   // State
-  const attributeList = ref([] as Attribute[])
+  const attributeList = ref({} as any)
 
   // Getters
   const forEntity = computed(() => (entityID: string | string[]) => {
-    return attributeList.value.filter((object) => object.entityID === entityID)
+    return attributeList.value.find({ entityID })
   })
 
   const find = computed(() => (entityID: string | string[], attributeDefinitionID: string | string[]) => {
-    return forEntity.value(entityID).find(attribute => attribute.attributeDefinitionID === attributeDefinitionID)
+    return attributeList.value.find({ entityID, attributeDefinitionID })[0]
   })
 
   // Actions
-  function initStore() { // TODO: Limit load to Entities for current DB.
-    attributesData = db.getCollection('attributes')
+  function initStore() {
+    attributeList.value = db.getCollection('attributes')
 
-    if(!attributesData){
-      attributesData = db.addCollection('attributes', { unique: ['id'], indices: ['id', 'entityID', 'attributeDefinitionID'], autoupdate: true })
+    if(!attributeList.value){
+      attributeList.value = db.addCollection('attributes', { unique: ['id'], indices: ['id', 'entityID', 'attributeDefinitionID'], autoupdate: true })
     }
-
-    let data = attributesData.data // TODO: Limit load to Entities for current DB.
-
-    attributeList.value = _unionBy(attributeList.value, data, 'id')
   }
 
   function addOrUpdateAttribute(value: string | number, entityID: string | string[], attributeDefinitionID: string | string[]) {
-    let attribute = attributesData.find({ entityID, attributeDefinitionID })[0]
+    let attribute = attributeList.value.find({ entityID, attributeDefinitionID })[0]
 
     if(!attribute){
-      attribute = attributesData.insert({ value, id: uuidv4(), entityID, attributeDefinitionID })
+      attribute = attributeList.value.insert({ value, id: uuidv4(), entityID, attributeDefinitionID })
     } else {
       attribute.value = value
-      attribute = attributesData.update(attribute)
+      attribute = attributeList.value.update(attribute)
     }
-
-    attributeList.value.push(attribute)
   }
 
   function deleteAttribute(itemID: string) {
-    attributesData.chain().find({ id: itemID }).remove()
-    attributeList.value = attributeList.value.filter((object) => {
-      return object.id !== itemID
-    })
+    attributeList.value.chain().find({ id: itemID }).remove()
   }
 
   return { attributeList, find, forEntity, initStore, addOrUpdateAttribute, deleteAttribute }
